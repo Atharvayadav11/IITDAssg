93786a61e99952008d9f48c9d0063aa028ae5a66
Atharva Yadav <atharvayadav11@gmail.com>
2025-03-02 01:00:46 +0530

    fixed some bugs

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
 import { 
   fetchDomains, 
